9e4a540b067b87e644857293cf7d5cc2ecb188b7
Gwen <gt.santus@gmail.com>
2024-05-08 10:04:48 +0100

    Test 1 - fails

--- src/test/java/com/secretdiary/test/diary/TestSecretDiary.java
+++ src/test/java/com/secretdiary/test/diary/TestSecretDiary.java
@@ -1,7 +1,11 @@
 package com.secretdiary.test.diary;
+
+import com.secretdiary.app.Diary;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestSecretDiary {
     @Nested
@@ -11,8 +15,11 @@ public class TestSecretDiary {
         @Test
         public void testWriteEntrySuccessfullyAddsValidInput(){
             //Arrange
+            Diary testDiary = new Diary();
             //Act
+            Diary.write("This is a test entry!");
             //Assert
+            assertEquals("This is a test entry!", Diary.readEntry());
         }
 
         @Test
